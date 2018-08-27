4fab10657d98f977450181cf49a436f65aebd9fc
AlexE <zed-chi@mail.ru>
2018-08-27 22:46:34 +0300

    changed print_repo_info
    removed issues fetching

--- github_trending.py
+++ github_trending.py
@@ -13,48 +13,44 @@ def get_start_date(day_shift=7):
     ).isoformat()
 
 
-def get_decoded_repos_json(page, date):
+def get_repos(date):
     url = "https://api.github.com/search/repositories"
     params = {
         "q": "created:>={}".format(date),
         "sort": "stars",
         "order": "desc",
-        "page": page
     }
     return requests.get(url, params=params).json()
 
 
 def get_trending_repositories(date, top_size):
-    page = 1
-    num_of_repos_to_load = top_size
-    repos = []
-    while num_of_repos_to_load > 0:
-        res = get_decoded_repos_json(page, date)
-        repos += res["items"]
-        num_of_repos_to_load -= len(res["items"])
-        if not res["items"] or len(res["items"]) < 30:
-            break
-        else:
-            page += 1
+    repos = get_repos(date)["items"]
+    if not repos:
+        return []
+    elif len(repos)<top_size:
+        return repos
     return repos[:top_size]
 
 
-def get_open_issues_amount(repo):
-    url = "{}/issues".format(repo["url"])
-    return requests.get(url, params={"state": "open"}).json()
-
-
-def print_rep_info(repos):
+def get_open_issues_amount(repos):
+    repos_issues = []
     for repo in repos:
+        url = "{}/issues".format(repo["url"])
+        issues = requests.get(url, params={"state": "open"}).json()
+        repos_issues.append(issues)
+    return repos_issues
+
+
+def print_repo_info(repos_with_issues):
+    for repo, issues in repos_with_issues:
         repo_name = repo["name"]
         stars = repo["stargazers_count"]
-        open_issues = get_open_issues_amount(repo)
         print("=> {}({} stars) - {} open issue(s):".format(
             repo_name,
             stars,
-            len(open_issues)),
+            len(issues)),
         )
-        print("\n".join([issues["url"] for issues in open_issues]), "\n")
+        print("\n".join(issue["url"] for issue in issues), "\n")
 
 
 def main():
@@ -64,7 +60,8 @@ def main():
         start_date,
         num_of_repos_to_print,
     )
-    print_rep_info(trending_repos)
+    issues = get_open_issues_amount(trending_repos)
+    print_repo_info(zip(trending_repos, issues))
 
 
 if __name__ == "__main__":
